46b1f684b0c065e3a11a2a4b09c34c95cdcf710c
Rajib Kumar Maity <57479702+Rajibcoder@users.noreply.github.com>
2023-09-09 17:56:40 +0530

    Update TestRunner.java

--- CucumberMyframeworkTest/src/test/java/runners/TestRunner.java
+++ CucumberMyframeworkTest/src/test/java/runners/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		features = {"src/test/resources/Login.feature","src/test/resources/Search.feature"},
-		glue = "stepDefinition"
+		glue = "stepDefinition",
+		plugin = {"json:target/cucumber-report.json"}
 		)
 public class TestRunner extends AbstractTestNGCucumberTests{
 
